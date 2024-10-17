a168d9bce69dfd0ae0e9e0a8f8f3c0d13bddbe86
iyeonghun <twosoull@naver.com>
2024-10-17 23:19:33 +0900

    first commit

---
+++ src/main/java/com/lookback/LookbackApplication.java
@@ -0,0 +1,13 @@
+package com.lookback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lookback/LookbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lookback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
