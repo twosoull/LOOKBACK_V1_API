70d6e9ac6ebe4061303ab3a2e908388aa43e70ae
iyeonghun <twosoull@naver.com>
2025-03-04 23:58:09 +0900

    :sparkles: jwt 리프레시토큰 적용

--- src/main/java/com/lookback/common/config/WebConfig.java
+++ src/main/java/com/lookback/common/config/WebConfig.java
@@ -1,11 +1,16 @@
 package com.lookback.common.config;
 
 import com.lookback.presentation.common.interceptor.JwtTokenInterceptor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -17,17 +22,17 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("Authorization", "Content-Type") // ✅ Authorization 허용
-                        .allowCredentials(true);
-            }
-        };
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트엔드 주소 지정
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type","Refresh-Token")); // Authorization 헤더 허용
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // OPTIONS 허용
+        config.setMaxAge(3600L); // Preflight 결과 캐싱 (1시간)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     @Override
